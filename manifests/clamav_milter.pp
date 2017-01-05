2c66c682715579f36326cf3df191053dd9f4946a
Ugo Bellavance <ugob@lubik.ca>
2017-01-05 14:33:25 -0500

    Added fail if OS is not RHEL 7+

--- manifests/clamav_milter.pp
+++ manifests/clamav_milter.pp
@@ -4,6 +4,10 @@
 
 class clamav::clamav_milter {
 
+  unless ($::osfamily == 'RedHat') and (versioncmp($::operatingsystemrelease, '7.0') >= 0) {
+    fail("OS family ${::osfamily}-${::operatingsystemrelease} is not supported. Only RedHat >= 7 is suppported.")
+  }
+
   $config_options = $clamav::_clamav_milter_options
 
   package { 'clamav_milter':
