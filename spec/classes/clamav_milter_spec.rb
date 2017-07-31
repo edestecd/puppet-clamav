651194f33d049371658c951867102cb5f9ffb67b
Ugo Bellavance <ubellavance@atq.qc.ca>
2017-07-31 18:16:35 -0400

    Added tests for clamav-milter

---
+++ spec/classes/clamav_milter_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'clamav', :type => :class do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge(:environment => 'test')
+      end
+
+      context 'with defaults' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.not_to contain_class('clamav::clamav_milter') }
+      end
+
+      context 'manage clamav_milter' do
+        let(:params) { { :manage_clamav_milter => true } }
+        it { is_expected.to contain_class('clamav::clamav_milter') }
+        context 'with defaults' do
+          it { is_expected.to contain_package('freshclam') }
+          it { is_expected.to contain_file('clamav-milter.conf') }
+          it { is_expected.to contain_service('clamav-milter') }
+        end
+      end
+    end
+  end
+end
