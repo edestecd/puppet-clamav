ad6b79007fc1eb801c382b57cbe5de784fe7e9a8
Simon Hoenscheid <simon.hoenscheid@helios-kliniken.de>
2018-07-04 15:59:08 +0200

    put variables in correct nameapace

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,9 +8,9 @@ class clamav::install {
     ensure => $clamav::clamav_version,
     name   => $clamav::clamav_package,
   }
-  if $additional_clamav_packages {
-    package { $additional_clamav_packages:
-      ensure => $additional_clamav_packages_version,
+  if $clamav::additional_clamav_packages {
+    package { $clamav::additional_clamav_packages:
+      ensure => $clamav::additional_clamav_packages_version,
     }
   }
 }
